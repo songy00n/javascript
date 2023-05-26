e61069a534e4e04b4d895879cb720bef66eaa4e8
songy00n <131989040+songy00n@users.noreply.github.com>
2023-05-26 14:28:34 +0900

    closure

--- closure3.js
+++ closure3.js
@@ -13,3 +13,41 @@ function makeWorker() {
   
   // call it
   work(); 
+
+
+  function makeCounter(){
+    let count = 0;
+    return function(){
+        return count++;
+    };
+  }
+
+  let counter = makeCounter();
+  alert (counter()); // 0 
+  alert (counter()); // 1
+  alert (counter()); // 2
+
+  let counter1 = makeCounter(); // (독립적인) 다른 렉시컬 환경을 가지고있음
+  alert (counter()); // 0
+  alert (counter()); // 1
+  alert (counter()); // 2
+
+  function Counter(){
+    let count = 0;
+
+    this.up = function(){
+        return count++;
+    }
+
+    this.down = function(){
+        return -- count;
+    }
+
+    alert (counter.up()); // 0 
+    alert (counter.up()); // 1
+    alert (counter.up()); // 2
+    alert (counter.down());// 1
+    alert (counter.down()); // 0
+    alert (counter.down()); // -1
+  }
+
