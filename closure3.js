915f0c4eb775b9fb583245ea9bfb914438ad0dd6
songy00n <131989040+songy00n@users.noreply.github.com>
2023-05-26 14:15:53 +0900

    closure

---
+++ closure3.js
@@ -0,0 +1,15 @@
+function makeWorker() {
+    let name = "Pete";
+  
+    return function() {
+      alert(name);
+    };
+  }
+  
+  let name = "John";
+  
+  // create a function
+  let work = makeWorker();
+  
+  // call it
+  work(); 
